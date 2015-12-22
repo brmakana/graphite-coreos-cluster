93f666a53efc600a59eb20fd43f23cba98601b97
Branden Makana <bmakana@cobalt.com>
2015-12-21 17:14:33 -0800

    Adding files necessary for public relay

--- carbon_relay/bin/boot.sh
+++ carbon_relay/bin/boot.sh
@@ -9,12 +9,14 @@ export ETCD=$HOST_IP:4001
 
 echo "[carbon-relay] booting container. ETCD: $ETCD"
 
-# Loop until confd has updated the config
-until confd -onetime -node $ETCD -config-file /etc/confd/conf.d/carbon.toml; do
-  echo "[carbon-relay] waiting for confd to refresh carbon.conf (waiting for nodes to be available)"
-  sleep 5
-done
+# Loop until confd has updated the config, unless we're overriding that
+if [[ -z "$IGNORE_ETCD" ]]
+then
+	until confd -onetime -node $ETCD -config-file /etc/confd/conf.d/carbon.toml; do
+	  echo "[carbon-relay] waiting for confd to refresh carbon.conf (waiting for nodes to be available)"
+	  sleep 5
+	done
+fi
 
-# Start logstash
 echo "[carbon-relay] starting carbon-relay"
 /var/lib/graphite/bin/carbon-relay.py --debug start
